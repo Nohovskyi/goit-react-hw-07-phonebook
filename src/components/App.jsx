9f0061d0a6464638588eae1b9fb9f918ec59d3aa
Nohovskyi <nogovskiy@gmail.com>
2023-04-02 12:09:46 +0300

    Додає готовий Арр

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import  Loader  from './Loader/Loader';
+import { getError, getIsLoading } from 'redux/selectors';
+import css from '././App.module.css';
+
 export const App = () => {
+  	const dispatch = useDispatch();
+    const isLoading = useSelector(getIsLoading);
+    const error = useSelector(getError);
+
+    useEffect(() => {
+      dispatch(fetchContacts());
+    }, [dispatch]);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.wrap}>
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm />
+      <Filter />
+      {isLoading && <Loader />}
+      {error && <p>{error}</p>}
+      <ContactList />
     </div>
   );
 };
